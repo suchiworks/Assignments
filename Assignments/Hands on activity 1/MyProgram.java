ab25df9546171901e670249a0df88dbc2acac4ff
suchiworks <111557185+suchiworks@users.noreply.github.com>
2022-09-30 12:47:20 +0530

    Add files via upload

---
+++ Assignments/Hands on activity 1/MyProgram.java
@@ -0,0 +1,11 @@
+package sep30Assg;
+
+public class MyProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My First Java Program");
+	 
+	}
+
+}
