dc6b8b9e2b6a2433aaee542ec8a4690960e98e38
suchiworks <111557185+suchiworks@users.noreply.github.com>
2022-09-30 12:50:31 +0530

    Add files via upload

---
+++ Assignments/Hands on Activity 2/MyProgram1.java
@@ -0,0 +1,12 @@
+package sep30Assg;
+
+public class MyProgram1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  DisplayMessage Mssg=new DisplayMessage();
+		    Mssg.printMessage();
+	  
+	}
+
+}
---
+++ Assignments/Hands on Activity 2/message.java
@@ -0,0 +1,11 @@
+package sep30Assg;
+/*Problem Statement:
+Fix the compilation error and invoke
+ the method printMessage() of DisplayMessage class.*/
+
+class DisplayMessage{
+	void printMessage(){
+		   System.out.println("Hello My Message");
+		}
+		}  
+		  
