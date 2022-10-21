92d3e7ba1cd019a748f2e42a62f35d2f0ba163e4
suchiworks <111557185+suchiworks@users.noreply.github.com>
2022-10-21 12:34:12 +0530

    Add files via upload

---
+++ 21-0ct-22/FutureInvestment.java
@@ -0,0 +1,11 @@
+package oct21Assg;
+
+public class FutureInvestment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float P =1000, R =10, T =5;
+		  float SI =(P * T * R)/100;        
+		  System.out.println("Simple interest ="+SI);
+    }
+}
