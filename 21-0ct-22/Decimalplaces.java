bad8debd24314898310443b6d719a41356803172
suchiworks <111557185+suchiworks@users.noreply.github.com>
2022-10-21 12:53:43 +0530

    Update Decimalplaces.java

--- 21-0ct-22/Decimalplaces.java
+++ 21-0ct-22/Decimalplaces.java
@@ -1,7 +1,8 @@
 package oct21Assg;
-/*.Write a Java program that reads in two floating-point 
-numbers and tests whether they are the same up to 
-three decimal places*/
+       /*.Write a Java program that reads in two floating-point 
+       numbers and tests whether they are the same up to 
+       three decimal places*/
+
 import java.util.Scanner;
 
 public class Decimalplaces {
@@ -13,11 +14,10 @@ System.out.print("Input floating-point number: ");
         float n1=(float) in.nextDouble();
         System.out.print("Input floating-point another number: ");
         float n2=(float) in.nextDouble();	        
-	 System.out.println(n1); 
 		      n1=Math.round(n1*1000);
               n1=n1/1000;
-n1=Math.round(n2*1000);
-n1=n1/1000;
+n2=Math.round(n2*1000);
+n2=n2/1000;
         if(n1==n2){
          System.out.println("They are the same up to three decimal places");}
         else
